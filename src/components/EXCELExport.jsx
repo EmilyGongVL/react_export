a8c79577653d3e51dfc030c8b62a707a8cd119f7
Emily Gong <emily.gong@vivaleisure.com.au>
2022-09-20 15:14:33 +1000

    fix: minimize import size

--- src/components/EXCELExport.jsx
+++ src/components/EXCELExport.jsx
@@ -1,11 +1,11 @@
-import * as XLSX from 'xlsx'
+import {utils, writeFile} from 'xlsx'
 
 export default function EXCELExport(props) {
 const exportToExcel = () => {
-  const wb = XLSX.utils.book_new(),
-  ws = XLSX.utils.aoa_to_sheet(props.data);
-  XLSX.utils.book_append_sheet(wb, ws, "MySheet1");
-  XLSX.writeFile(wb, "MyExcel.xlsx");
+  const wb = utils.book_new(),
+  ws = utils.aoa_to_sheet(props.data);
+  utils.book_append_sheet(wb, ws, "MySheet1");
+  writeFile(wb, "MyExcel.xlsx");
 }
   return (
     <button onClick={exportToExcel}>Excel</button>
